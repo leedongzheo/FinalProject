44d1f49101e0071e0954b641511e8cc87e042e35
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2023-05-28 19:20:13 +0700

    Update Thuchanh.py

--- pages/Thuchanh.py
+++ pages/Thuchanh.py
@@ -301,7 +301,8 @@ def download(image,file_uploaded):
     st.sidebar.markdown(href, unsafe_allow_html=True)
     
 def display_image(column, img, caption):
-    column.image(img, caption, use_column_width=True,caption_style=f"color: white;")
+    caption_style = f"color: white;"
+    column.image(img, caption, use_column_width=True, caption_style=caption_style)
 
 if __name__ == "__main__":
     main()
